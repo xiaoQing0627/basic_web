ec48d3ada3bac82d539b87afde7ffeeee8774f8f
qing <xiaoqing@163.com>
2017-06-22 18:00:14 +0800

    convenienceV1.1

---
+++ demo/demoCompress/main.js
@@ -0,0 +1,62 @@
+/**
+ * Created by Administrator on 2017/6/19 0019.
+ */
+/*
+ *移动端图片压缩上传功能后台
+ */
+"use strict";
+
+var fs = require('fs');
+var router = require("../router");
+//var FormParser = require("./formParser");
+var formidable = require('formidable');
+var path = require('path');
+
+var fileSaveDir = path.join(STATIC_PATH, 'upload');
+
+router.setMap({
+    "uindex_2": path.join(__dirname, "./index_2.html"),
+    "cupload": cupload
+});
+
+function cupload(req, res) {
+    if (!fs.existsSync(fileSaveDir)) {
+        fs.mkdirSync(fileSaveDir)
+    }
+
+    var form = new formidable.IncomingForm();
+    var responseData = [];
+    form.uploadDir = fileSaveDir;
+    form.type = true;
+    form.keepExtensions = true;
+
+    form.parse(req, function(err, fields, files){
+        if(!err) {
+            Object.keys(files).forEach(function(key){
+                var file = files[key];
+                var filename = path.basename(file.path);
+
+                //每张图片给予一分钟保存时间
+                setTimeout(function() {
+                    if (!fs.existsSync(file.path)) return;
+
+                    console.log("\x1B[33m删除文件" + filename + "\x1B[0m");
+                    fs.unlinkSync(file.path);
+                }, 60 * 1000);
+
+                // 塞入响应数据中
+                responseData.push({
+                    type: file.type,
+                    name: filename,
+                    path: '/public/upload/' + filename,
+                    size: file.size / 1024 > 1024 ? (~~(10 * file.size / 1024 / 1024)) / 10 + "MB" : ~~(file.size / 1024) + "KB"
+                });
+            });
+        } else {
+            console.warn(err);
+        }
+
+        res.writeHead(200);
+        res.end(JSON.stringify(responseData));
+    });
+}
